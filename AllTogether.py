79a2b02f758565390bc22cbc1748b7d26c453339
Ellie Marsden <ejam2@kent.ac.uk>
2024-10-23 10:24:49 +0100

    Edited Part 4

--- AllTogether.py
+++ AllTogether.py
@@ -2,13 +2,13 @@
 import random
 
 list = []
-outputlist = []
+
 for i in range(10):
     list.append(random.randint(1,100))
     
-
-for number in list:
-    while number % 2 == 1 and number not in list:
-        outputlist.append(number)
-                   
-print(f"The output list of odd numbers is: {str(outputlist)}.")
+# Creates a copy of the list (can be used in iterating while removing items) from Python Engineer: https://www.python-engineer.com/posts/remove-elements-in-list-while-iterating/
+# The syntax is {list name}[:]
+for number in list[:]:
+    while number % 2 == 0 and number in list:
+        list.pop(list.index(number))
+print(f"The output list of odd numbers is: {str(list)}.")
